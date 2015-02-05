aadccca89ce43e55124c359d70e3d301a7d17572
Johannes Ernst <jernst@indiecomputing.com>
2015-02-05 19:11:41 +0000

    make sure strict/warnings are on everywhere fixed typo that caused no op on PC disks added copyright / license headers

--- ubos-install/vendor_perl/UBOS/Install/DiskLayouts/DiskImage.pm
+++ ubos-install/vendor_perl/UBOS/Install/DiskLayouts/DiskImage.pm
@@ -1,7 +1,26 @@
 #
 # A DiskImage disk layout. Contains at least one partition. May contain
 # boot sector.
+# 
+# This file is part of ubos-install.
+# (C) 2012-2015 Indie Computing Corp.
 #
+# ubos-install is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ubos-install is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with ubos-install.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+use strict;
+use warnings;
 
 package UBOS::Install::DiskLayouts::DiskImage;
 
