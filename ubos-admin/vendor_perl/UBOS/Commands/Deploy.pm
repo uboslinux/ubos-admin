3bc7fe3161bcf4ea4d6456341a94c50fc424836d
Johannes Ernst <jernst@indiecomputing.com>
2017-12-22 22:02:24 +0000

    Incorrect variable

--- ubos-admin/vendor_perl/UBOS/Commands/Deploy.pm
+++ ubos-admin/vendor_perl/UBOS/Commands/Deploy.pm
@@ -244,7 +244,7 @@ sub run {
             my $appPackage = $newAppConfig->app()->packageName();
             foreach my $acc ( $newAppConfig->accessories() ) {
                 if( $appPackage ne $acc->belongsToApp() ) {
-                    fatal( 'Accessory', $acc->packageName(), 'cannot be used in appconfig', $newAppConfigId, 'as it does not belong to app', $appPackage );
+                    fatal( 'Accessory', $acc->packageName(), 'cannot be used in appconfig', $newAppConfig->appConfigId(), 'as it does not belong to app', $appPackage );
                 }
             }
         }
