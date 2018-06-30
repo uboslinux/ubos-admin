e6cf9a76914695b33a2a04316783aa3ead50dcf9
Johannes Ernst <jernst@indiecomputing.com>
2018-06-29 19:10:04 -0700

    Regex also needs to match singular "user" not just "users"

--- ubos-admin/vendor_perl/UBOS/Commands/Status.pm
+++ ubos-admin/vendor_perl/UBOS/Commands/Status.pm
@@ -204,7 +204,7 @@ MSG
             my @lines = split /\n/, $out;
             my $first = shift @lines;
 
-            if( $first =~ m!^\s*(\d+:\d+:\d+)\s*up\s*([^,]+),\s*(\d+)\s*users,\s*load\s*average:\s*([^,]+),\s*([^,]+),\s*([^,]+)\s*$! ) {
+            if( $first =~ m!^\s*(\d+:\d+:\d+)\s*up\s*([^,]+),\s*(\d+)\s*users?,\s*load\s*average:\s*([^,]+),\s*([^,]+),\s*([^,]+)\s*$! ) {
                 $json->{uptime}->{time}      = $1;
                 $json->{uptime}->{uptime}    = $2;
                 $json->{uptime}->{nusers}    = $3;
