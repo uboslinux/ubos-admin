8e3bb35d3edd4eb7bc6c9ab36e24982e85558a09
Johannes Ernst <jernst@indiecomputing.com>
2019-07-24 21:59:14 +0000

    Also create/update /etc/issue.net per #648

--- ubos-perl-utils/vendor_perl/UBOS/Utils.pm
+++ ubos-perl-utils/vendor_perl/UBOS/Utils.pm
@@ -1162,11 +1162,15 @@ ISSUE
     $issue .= <<ISSUE;
 +--------------------------------------------------------------------------+
 
+ISSUE
+
+    my $advice = <<ADVICE;
 Note: run 'sudo ubos-admin update' to get the latest version.
       and: frequent backups with 'sudo ubos-admin backup' are recommended.
 
-ISSUE
-    UBOS::Utils::saveFile( $target . '/etc/issue', $issue, 0644, 'root', 'root' );
+ADVICE
+    UBOS::Utils::saveFile( $target . '/etc/issue',     $issue . $advice, 0644, 'root', 'root' );
+    UBOS::Utils::saveFile( $target . '/etc/issue.net', $issue,           0644, 'root', 'root' );
 }
 
 ##
