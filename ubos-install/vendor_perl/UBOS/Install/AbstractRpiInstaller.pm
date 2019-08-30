3a68e2bb4276ab3daf62ffa7c31cd3d2f13eaf7a
Johannes Ernst <jernst@indiecomputing.com>
2019-08-30 18:46:14 +0000

    Invalid device class dependency on RPI2

--- ubos-install/vendor_perl/UBOS/Install/AbstractRpiInstaller.pm
+++ ubos-install/vendor_perl/UBOS/Install/AbstractRpiInstaller.pm
@@ -43,7 +43,7 @@ sub new {
         $self->{devicepackages} = [ qw( ubos-networking-client
                 raspberrypi-firmware raspberrypi-bootloader
                 raspberrypi-bootloader-x archlinuxarm-keyring
-                rng-tools smartmontools wpa_supplicant crda ubos-deviceclass-rpi ) ];
+                rng-tools smartmontools wpa_supplicant crda ) ];
     }
     unless( $self->{deviceservices} ) {
         $self->{deviceservices} = [ qw( rngd.service systemd-timesyncd.service ) ];
